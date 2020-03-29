e3271c8b9b81832e9e591d69c39f5b0150b7ad84
Kevin Dang <mrkevindang@gmail.com>
2020-03-28 18:44:29 -0700

    Initial check-in

---
+++ client/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Danae's Page</h1>
+    <div><img alt="Vue logo" src="../assets/tumblr_puedgmfaEL1vvk8dyo1_1280.png"></div>
+    <div><img alt="Vue logo" src="../assets/tumblr_ptmubnSZgT1vvk8dyo1_1280.png"></div>
+    <div><img alt="Vue logo" src="../assets/tumblr_pttutgF5tS1vvk8dyo1_1280.png"></div>
+  </div>
+</template>
+
+<script>
+
+</script>
